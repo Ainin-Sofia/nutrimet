f3caab39f2ee0557cf3fe6a5916cbd635118c3e0
NandaDwiCahyo <nandad.cahyo@gmail.com>
2023-12-31 17:25:41 +0700

    Menambahkan controller StatusGiziController dan membuat view index status gizi

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\StatusGiziController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/status_gizi', [StatusGiziController::class, 'index']);
