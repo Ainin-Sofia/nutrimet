619fff027e10785f6176e3b5098ad54bce70c8a8
NandaDwiCahyo <nandad.cahyo@gmail.com>
2024-01-17 16:25:48 +0700

    Membuat migrasi tabel standart

---
+++ database/migrations/2024_01_16_113203_create_pre_metabolic_syndrome_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pre_metabolic_syndrome', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->date('tanggal_cek');
+            $table->integer('tekanan_darah');
+            $table->integer('lingkar_pinggang');
+            $table->string('gula_darah');
+            $table->integer('hdl');
+            $table->integer('trigliserida');
+            $table->string('hasil_status_pms');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pre_metabolic_syndrome');
+    }
+};
